56684a1dc706b69292d4d3ab33e029160b4aadbf
Kneve327 <kneve327@gmail.com>
2018-09-27 19:04:11 -0400

    pushing

--- README.md
+++ README.md
@@ -1,6 +1,7 @@
 # TriviaGame
 
 <!-- Yeah I definitely didn't finish.
-I would have tried to link the answer id to how many they got correct.
 It would say the amount the user got wrong and right.
-Also the spacing at the bottom of the container wasn't working right. -->
+Also the spacing at the bottom of the container wasn't working right.
+But most importantly, it's no longer changing pages when the "done" button is clicked.
+I don't know how else to fix that -->
--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,7 +1,9 @@
 var number = 120;
 var interval;
+var count = 0;
+var wrong = 0;
+var right = 0;
 
-$("#done").on("click", stop);
 $("#startButton").on("click", run);
 
 function run() {
@@ -25,12 +27,31 @@ function decrement() {
     }
 }
 
+$("#done").on("click", stop);
+
+
 function stop() {
 
     clearInterval(interval);
-
     location.replace("fourthpage.html")
 
+    right = 0;          
+    for(var i = 1; i <= 15; i++) {
+  var radios = document.getElementsByName('group'+i);
+  for(var j = 0; j < radios.length; j++) {
+    var radio = radios[j];
+    if(radio.value == "right" && radio.checked) {
+      right++;
+    }
+  }
+ } 
+
+    $("#wrong").html("Incorrect: " + wrong);
+    $("#right").html("Correct: " + right);
 }
 
+
+
+
 run();
+
